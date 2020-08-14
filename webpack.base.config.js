cb79cafa2907f37f334c54d3872c40efc978b7eb
alexwooder <alexwooder@163.com>
2020-08-14 23:42:34 +0800

    提交配置文件

---
+++ webpack.base.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/main',
+        vendors: './src/vendors'
+    },
+    output: {
+        path: path.join(__dirname, './dist')
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: "./",
+        quiet: false, //控制台中不输出打包的信息
+        noInfo: false,
+        hot: true, //开启热点
+        inline: true, //开启页面自动刷新
+        lazy: false, //不启动懒加载
+        progress: true, //显示打包的进度
+        watchOptions: {
+            aggregateTimeout: 300
+        },
+        port: '8081'
+    },
+    module: {
+        rules: [{
+                test: /.vue$/,
+                use: [{
+                        loader: 'vue-loader',
+                        options: {
+                            loaders: {
+                                less: ExtractTextPlugin.extract({
+                                    use: ['css-loader?minimize', 'autoprefixer-loader', 'less-loader'],
+                                    fallback: 'vue-style-loader'
+                                }),
+                                css: ExtractTextPlugin.extract({
+                                    use: ['css-loader', 'autoprefixer-loader', 'less-loader'],
+                                    fallback: 'vue-style-loader'
+                                })
+                            }
+                        }
+                    }
+                ]
+            },
+           
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader?minimize', 'autoprefixer-loader'],
+                    fallback: 'style-loader'
+                })
+            },
+
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=1024&name=imgs/[name].[hash].[ext]'
+            },
+            {
+                test: /\.(html|tpl)$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+   
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            'vue': 'vue/dist/vue.esm.js'
+        }
+    }
+};
